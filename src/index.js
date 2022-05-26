ae7758725a2192b3da2f57560f9e3fa86bb95099
hyunsoo <qkrqudlas@gmail.com>
2022-05-26 16:25:18 +0900

    todoapp

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import { log } from './utils';
 import './todos.css';
 
+
+const $form = document.querySelector('.new-task');
+form.addEventListener('submit',(event)=>{
+
+    event.preventDefault();
+    console.log
+});
+
 log('webpack5 boilerplate');
