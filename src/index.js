a98715798bedd9d8c045ba441601f1ccae15344a
hyunsoo <qkrqudlas@gmail.com>
2022-05-26 16:39:45 +0900

    todos 기본소스

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import { log } from './utils';
 import './todos.css';
 
+const todos  = [{title:'놀기'}, {title:'먹기'}];
 
+
+//폼, iput 입력 이벤트 추가하기
+//입력했을때 todo 추가
 const $form = document.querySelector('.new-task');
+const $input = document.querySelector('.new-task > input');
+const $todos = document.querySelector('#todos');
+
+const printTodos
+
+const checkTodo = (event) => {
+    event.preventDefault();
+    console.log($input.value);
+};
+
 form.addEventListener('submit',(event)=>{
 
     event.preventDefault();
