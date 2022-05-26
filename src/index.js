624de77b45d6236d2a2444076100a45817802ea8
hyunsoo <qkrqudlas@gmail.com>
2022-05-26 16:42:48 +0900

    todoapp

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
 import { log } from './utils';
 import './todos.css';
 
-const todos  = [{title:'놀기'}, {title:'먹기'}];
+const todos = [
+  { title: 'HTML', isDone: true },
+  { title: 'JS', isDone: true },
+  { title: 'React', isDone: true },
+];
 
-
-//폼, iput 입력 이벤트 추가하기
-//입력했을때 todo 추가
+//
+// 폼,input 입력 이벤트 추가하기
+// 입력했을때 todo추가
 const $form = document.querySelector('.new-task');
 const $input = document.querySelector('.new-task > input');
 const $todos = document.querySelector('#todos');
 
-const printTodos
+const printTodos = () => {
+  const html = todos.map((todo) => {
+    return `
+		<li>
+			<button class="delete">×</button>
+			<input type="checkbox" class="toggle-checked" />
+			<span class="text">${todo.title}</span>
+		</li>
+		`;
+  });
 
-const checkTodo = (event) => {
-    event.preventDefault();
-    console.log($input.value);
+  $todos.innerHTML = `<ul>${html.join('')}</ul>`;
 };
 
-form.addEventListener('submit',(event)=>{
+const checkTodo = (event) => {
+  event.preventDefault();
+  console.log($input.value);
 
-    event.preventDefault();
-    console.log
-});
+  // todo 추가
+  const todo = { title: $input.value, isDone: false };
+  todos.push(todo);
+  console.log(todos);
 
-log('webpack5 boilerplate');
+  // input 초기화
+  $input.value = '';
+
+  // todos 출력
+  printTodos();
+};
+
+$form.addEventListener('submit', checkTodo);
+
+// todos 출력
+printTodos();
